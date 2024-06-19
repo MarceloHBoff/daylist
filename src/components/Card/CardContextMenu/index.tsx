26fa85d29b3c479463716717fd3ca122870f37cc
Marcelo <marceloboff20@gmail.com>
2024-06-19 20:09:57 -0300

    Implements context menu on ticket

---
+++ src/components/Card/CardContextMenu/index.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import Image from 'next/image'
+import { useState } from 'react'
+
+const options = [
+  {
+    label: 'Duplicate',
+    icon: '/duplicate.svg'
+  },
+  {
+    label: 'Delete',
+    icon: '/delete.svg'
+  }
+]
+
+export default function CardContextMenu() {
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => setOpen(true)
+
+  const handleClose = () => setOpen(false)
+
+  return (
+    <div
+      className="relative"
+      onContextMenu={e => {
+        e.preventDefault()
+        handleOpen()
+      }}
+    >
+      <div
+        className="hidden group-hover:block cursor-pointer"
+        onClick={handleOpen}
+      >
+        <Image src="/more.svg" alt="More" width={24} height={24} />
+      </div>
+
+      {open && (
+        <div className="">
+          <ul
+            className="absolute bg-zinc-600 shadow-lg rounded-lg py-2 z-50 min-w-44"
+            style={{ top: 20, right: 20 }}
+          >
+            {options.map(item => (
+              <li
+                key={item.label}
+                className="px-4 py-2 text-white hover:bg-zinc-700 cursor-pointer flex items-center"
+                onClick={handleClose}
+              >
+                <Image
+                  src={item.icon}
+                  alt={item.label}
+                  width={18}
+                  height={18}
+                  className="opacity-75"
+                />
+
+                <span className="ml-4">{item.label}</span>
+              </li>
+            ))}
+          </ul>
+
+          <div className="fixed inset-0 z-40" onClick={handleClose} />
+        </div>
+      )}
+    </div>
+  )
+}
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,7 @@
 import { TicketWithTag } from '@/models/ticket'
 import TagIcon from '@/components/TagIcon'
 import CardCheck from './CardCheck'
+import CardContextMenu from './CardContextMenu'
 
 type CardProps = {
   ticket: TicketWithTag
@@ -8,12 +9,12 @@ type CardProps = {
 
 export default function Card({ ticket }: CardProps) {
   return (
-    <article className="bg-zinc-700 mx-2 my-3 p-3 w-full min-h-20 rounded-xl border-2 border-gray-600 flex">
+    <article className="bg-zinc-700 mx-2 my-3 p-3 w-full min-h-20 rounded-xl border-2 border-gray-600 flex group">
       <div className="flex items-center mr-2">
         <CardCheck id={ticket.id} />
       </div>
 
-      <div className="flex flex-col">
+      <div className="flex flex-col w-full">
         <div className="text-slate-300 text-sm font-bold">
           {ticket.description}
         </div>
@@ -31,6 +32,8 @@ export default function Card({ ticket }: CardProps) {
           </div>
         )}
       </div>
+
+      <CardContextMenu />
     </article>
   )
 }
