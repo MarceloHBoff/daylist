4021e8e1c14512d7d9d1e4aa9d14e37a048297ba
Marcelo <marceloboff20@gmail.com>
2024-08-06 18:52:10 -0300

    Fix date bug

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -6,8 +6,8 @@ export function getDateFilter(date: Date | null) {
   const currentDate = new Date(date ?? '')
 
   return {
-    gte: new Date(currentDate.setUTCHours(0, 0, 0, 0)),
-    lt: new Date(currentDate.setUTCHours(24, 0, 0, 0))
+    gte: new Date(currentDate.setUTCHours(0, 0, 0)),
+    lt: new Date(currentDate.setUTCHours(23, 59, 59))
   }
 }
 
