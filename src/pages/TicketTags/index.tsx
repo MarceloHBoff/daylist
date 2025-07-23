802fa377ee7018e8d27382c4455332a877761dae
Marcelo <marceloboff20@gmail.com>
2025-07-23 13:18:15 -0300

    feat: implements undated filter on tickets by tag

--- src/pages/TicketTags/index.tsx
+++ src/pages/TicketTags/index.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 import * as Ticket from '@/components/Ticket'
 import RequestError from '@/error/requestError'
@@ -20,6 +20,7 @@ export default function TicketTags() {
   const { loader } = useLoading()
 
   const [tickets, setTickets] = useState<TicketTagsType[]>([])
+  const [onlyUndated, setOnlyUndated] = useState(false)
 
   useEffect(() => {
     loader(async () => {
@@ -37,20 +38,45 @@ export default function TicketTags() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  const ticketsFiltered = useMemo(() => {
+    if (onlyUndated) {
+      return tickets.filter(p => p.data.some(i => !i.date))
+    }
+
+    return tickets
+  }, [onlyUndated, tickets])
+
   return (
     <div className="h-ticket-tags py-4">
-      <div className="mx-8 mb-4 border-b-2 border-b-slate-500 p-2 text-3xl font-bold text-white">
-        Ticket by Tags
+      <div className="mx-8 mb-4 flex justify-between border-b-2 border-b-slate-500 p-2 text-3xl font-bold text-white">
+        <span>Ticket by Tags</span>
+
+        <div className="me-4 flex items-center">
+          <input
+            id="green-checkbox"
+            type="checkbox"
+            checked={onlyUndated}
+            onChange={() => setOnlyUndated(!onlyUndated)}
+            className="h-8 w-8 cursor-pointer border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+          />
+          <label
+            htmlFor="green-checkbox"
+            className="ms-2 cursor-pointer text-lg font-medium text-gray-900 dark:text-gray-300"
+          >
+            Only undated
+          </label>
+        </div>
       </div>
 
       <article className="flex h-full overflow-x-auto">
-        {tickets.map(p => (
+        {ticketsFiltered.map(p => (
           <Ticket.TicketsWrapper
             key={p.key}
             title={p.key}
             defaultValues={{ tagId: p.data[0].tagId }}
           >
             {p.data
+              .filter(p => (onlyUndated ? !p.date : true))
               .sort((a, b) => {
                 if (!a.date) return 1
                 if (!b.date) return -1
